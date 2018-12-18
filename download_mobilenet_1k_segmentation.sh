9f5c58214e3c427573c32489e7c6768fcc98487c
medanyaYandex <medanya@yandex-team.ru>
2018-12-18 15:21:24 +0300

    add script for downloading dataset and model

---
+++ download_mobilenet_1k_segmentation.sh
@@ -0,0 +1,3 @@
+wget -O ./mobilenet_1k_segmentation.tar https://www.dropbox.com/s/ooe9eqd1u6tjvn9/mobilenet_1k_segmentation.tar?dl=0
+tar -xvf mobilenet_1k_segmentation.tar ./
+rm mobilenet_1k_segmentation.tar
